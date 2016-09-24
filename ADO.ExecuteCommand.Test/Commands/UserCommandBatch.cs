343053cabdd0f5e0a9b07cb6a9ae981d15a48bb7
omar <omar@odelvalle.com>
2016-09-24 11:55:19 +0100

    Integration test. Execute Batch Commands

---
+++ ADO.ExecuteCommand.Test/Commands/UserCommandBatch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using ADO.ExecuteCommand.Commands;
+
+namespace ADO.ExecuteCommand.Test.Commands
+{
+    public class UserCommandBatch: CommandBatch
+    {
+        public UserCommandBatch()
+        {
+            this.Commands = new List<Command>
+            {
+                new InsertCommandWithParameters(1, "Test batch"),
+                new UpdateCommandWithParameters(1, "update test batch")
+            };
+        }
+    }
+}
